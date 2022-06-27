58d0c24bdb94f19c44dfa337e2bc9a29c629949f
Søren Magnusson <soren@magnussons.dk>
2022-06-27 08:15:18 +0200

    apt update

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@
 
 
 
-mkdir poky
+mkdir poky && \
+cd poky
+
+sudo apt update && sudo apt upgrade
 
 # https://docs.yoctoproject.org/brief-yoctoprojectqs/index.html
 sudo apt-get install -y gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm python3-subunit mesa-common-dev zstd liblz4-tool
